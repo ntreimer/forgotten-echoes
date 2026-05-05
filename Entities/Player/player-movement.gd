b1b03b60e5793bb9293d4f071231edff28f548e9
Nate Treimer <76891451+ntreimer@users.noreply.github.com>
2026-05-04 21:41:53 -0500

    Directory organization, player init, level_01 init

---
+++ Entities/Player/player-movement.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var SPEED = 150.0
+@export var JUMP_VELOCITY = -300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		# Flip sprite
+		$AnimatedSprite2D.flip_h = direction < 0
+	else:
+		# move_toward adds friction; stop gradually
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Handles character movement and collisions
+	move_and_slide()
