54cef9c68c2ef3ce238e876d143b1345c9c349db
Nate Treimer <76891451+ntreimer@users.noreply.github.com>
2026-05-06 11:55:37 -0500

    Fix sprite flipping during jump

--- Entities/Player/PlayerFall.gd
+++ Entities/Player/PlayerFall.gd
@@ -6,7 +6,10 @@ func enter():
 func physics_update(delta):
 	player.velocity += player.get_gravity() * delta
 	var direction = Input.get_axis("move_left", "move_right")
-	player.velocity.x = direction * player.SPEED
+	
+	if direction:
+		player.velocity.x = direction * player.SPEED
+		player.sprite.flip_h = direction < 0
 	
 	player.move_and_slide()
 	
